a652ba89b8405cc96492356b717fe850141b42e4
Jacek Szulc <j.szulc77@wp.pl>
2024-09-01 15:34:25 +0200

    Projekt#8.3

--- js/main.js
+++ js/main.js
@@ -9,8 +9,26 @@ const weather = document.querySelector(".weather");
 const temperature = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 
-const apiLink = "https://api.openweathermap.org/data/2.5/weather?q=";
-const apiKey = "&appid=70a1b3128222ce7241d73e5a7fc49809";
-const units = "&units=metric";
-let city;
-let url;
+const $apiLink = "https://api.openweathermap.org/data/2.5/weather?q=";
+const $apiKey = "&appid=70a1b3128222ce7241d73e5a7fc49809";
+const $units = "&units=metric";
+let $city;
+let $url;
+
+const getWeather = () => {
+	// $city = input.value;
+	$city = "London";
+	$url = $apiLink + $city + $apiKey + $units;
+
+	axios.get($url).then(res => {
+		const temp = res.data.main.temp;
+		const hum = res.data.main.humidity;
+
+		cityName.textContent = res.data.name;
+		temperature.textContent = Math.floor(temp) + "° C";
+		humidity.textContent = hum + " %";
+
+		console.log(res.data);
+	});
+};
+getWeather();
