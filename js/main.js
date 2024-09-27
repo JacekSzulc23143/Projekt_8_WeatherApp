384d24be404ba49f026cf8e466c12a99f58c1a23
Jacek Szulc <j.szulc77@wp.pl>
2024-09-27 20:57:12 +0200

    Projekt#8.12 PL

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const btn = document.querySelector("button");
 const cityName = document.querySelector(".city-name");
 const warning = document.querySelector(".warning");
 const photo = document.querySelector(".photo");
+const clouds = document.querySelector(".clouds");
 
 const weather = document.querySelector(".weather");
 const temperature = document.querySelector(".temp");
@@ -23,22 +24,23 @@ console.log(`Godzina: ${$hours}:${$minutes}`);
 const getWeather = () => {
 	$city = !input.value ? "Gdynia" : input.value;
 	$url = $apiLink + $city + $apiKey + $units;
-
+	
 	axios
 		.get($url)
 		.then(res => {
 			const temp = res.data.main.temp;
 			const hum = res.data.main.humidity;
 			const status = Object.assign({}, ...res.data.weather);
-
+			
 			cityName.textContent = `${res.data.name} (${res.data.sys.country})`;
 			weather.textContent = status.main;
 			temperature.textContent = Math.floor(temp) + "° C";
 			humidity.textContent = hum + " %";
-
+			
 			warning.textContent = "";
+			clouds.textContent = "";
 			input.value = "";
-
+			
 			if (status.id >= 200 && status.id < 300) {
 				if (status.icon === "11d") {
 					photo.setAttribute("src", "img/11d.png");
@@ -47,6 +49,7 @@ const getWeather = () => {
 				}
 				// photo.setAttribute("src", "img/thunderstorm.png");
 				weather.textContent = "Burze";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 			} else if (status.id >= 300 && status.id < 400) {
 				if (status.icon === "09d") {
 					photo.setAttribute("src", "img/09d.png");
@@ -55,16 +58,21 @@ const getWeather = () => {
 				}
 				// photo.setAttribute("src", "img/drizzle.png");
 				weather.textContent = "Mżawka";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 			} else if (status.id >= 500 && status.id < 600) {
 				if (status.icon === "10d") {
 					photo.setAttribute("src", "img/10d.png");
+					weather.textContent = "Deszczowo";
 				} else if (status.icon === "10n") {
 					photo.setAttribute("src", "img/10n.png");
+					weather.textContent = "Deszczowo";
 				}
 				if (status.icon === "09d") {
 					photo.setAttribute("src", "img/09d.png");
+					weather.textContent = "Deszczowo";
 				} else if (status.icon === "09n") {
 					photo.setAttribute("src", "img/09n.png");
+					weather.textContent = "Deszczowo";
 				}
 				if (status.icon === "13d") {
 					photo.setAttribute("src", "img/13d.png");
@@ -74,7 +82,7 @@ const getWeather = () => {
 					weather.textContent = "Marznący deszcz";
 				}
 				// photo.setAttribute("src", "img/rain.png");
-				weather.textContent = "Deszczowo";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 			} else if (status.id >= 600 && status.id < 700) {
 				if (status.icon === "13d") {
 					photo.setAttribute("src", "img/13d.png");
@@ -83,9 +91,11 @@ const getWeather = () => {
 				}
 				// photo.setAttribute("src", "img/ice.png");
 				weather.textContent = "Śnieg";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 			} else if (status.id >= 700 && status.id < 800) {
 				if (status.icon === "50d") {
 					photo.setAttribute("src", "img/50d.png");
+					clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 					if (status.id == 701 || status.id == 721 || status.id == 741) {
 						weather.textContent = "Mgła";
 					}
@@ -109,6 +119,7 @@ const getWeather = () => {
 					}
 				} else {
 					photo.setAttribute("src", "img/50n.png");
+					clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 					if (status.id == 701 || status.id == 721 || status.id == 741) {
 						weather.textContent = "Mgła";
 					}
@@ -133,7 +144,7 @@ const getWeather = () => {
 				}
 				// photo.setAttribute("src", "img/fog.png");
 				// weather.textContent = "Mgła";
-			} else if (status.id === 800) {
+			} else if (status.id == 800) {
 				if (status.icon === "01d") {
 					// photo.setAttribute("src", "img/01d.png");
 					photo.setAttribute("src", "img/sun.png");
@@ -143,23 +154,26 @@ const getWeather = () => {
 					photo.setAttribute("src", "img/moon.png");
 					weather.textContent = "Pogodnie";
 				}
-			} else if (status.id === 801) {
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
+			} else if (status.id == 801) {
 				if (status.icon === "02d") {
 					photo.setAttribute("src", "img/02d.png");
 				} else {
 					photo.setAttribute("src", "img/02n.png");
 				}
 				// photo.setAttribute("src", "img/cloud.png");
-				weather.textContent = "Pochmurnie";
-			} else if (status.id === 802) {
+				weather.textContent = "Zachmurzenie małe";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
+			} else if (status.id == 802) {
 				if (status.icon === "03d") {
 					photo.setAttribute("src", "img/03d.png");
 				} else {
 					photo.setAttribute("src", "img/03n.png");
 				}
 				// photo.setAttribute("src", "img/cloud.png");
-				weather.textContent = "Pochmurnie";
-			} else if (status.id > 802 && status.id < 900) {
+				weather.textContent = "Zachmurzenie umiarkowane";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
+			} else if (status.id == 803) {
 				if (status.icon === "04d") {
 					photo.setAttribute("src", "img/04d.png");
 				} else {
@@ -167,16 +181,28 @@ const getWeather = () => {
 				}
 				// photo.setAttribute("src", "img/cloud.png");
 				weather.textContent = "Pochmurnie";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
+			} else if (status.id == 804) {
+				if (status.icon === "04d") {
+					photo.setAttribute("src", "img/04d.png");
+				} else {
+					photo.setAttribute("src", "img/04n.png");
+				}
+				// photo.setAttribute("src", "img/cloud.png");
+				weather.textContent = "Zachmurzenie duże";
+				clouds.textContent = `Chmury ${res.data.clouds.all} %`;
 			} else {
 				photo.setAttribute("src", "img/unknown.png");
 			}
 
 			console.log("---------------");
-			console.log(res.data.name);
 			// console.log(res.data);
+			console.log(res.data.name);
 			console.log(res.data.sys.country);
+			console.log(res.data.clouds.all);
+			console.log(`Chmury ${res.data.clouds.all} %`);
 			// console.log(res.data.sys);
-			console.log(weather.textContent);
+			// console.log(weather.textContent);
 			console.log(status);
 			console.log(status.id);
 			console.log(status.main);
